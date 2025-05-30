gaepdit/email-queue-service

Configure publishing to the web servers

EmailQueue.WebApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using EmailQueue.WebApp.Platform;
+using EmailQueue.WebApp.Platform;
 using EmailQueue.WebApp.Platform.DataProtection;
 using EmailQueue.WebApp.Services;
 

gaepdit/email-queue

EmailQueue.WebApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using EmailQueue.WebApp.Services;
+using EmailQueue.WebApp.Services;
 using EmailQueue.WebApp.Settings;
 
 var builder = WebApplication.CreateBuilder(args);