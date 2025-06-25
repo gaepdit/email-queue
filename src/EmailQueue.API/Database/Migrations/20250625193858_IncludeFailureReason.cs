26abcfcf827e9ed158a67d67155fa0a675eee8e8
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-06-25 16:26:13 -0400

    Log reasons for email failures (#41)

---
+++ src/EmailQueue.API/Database/Migrations/20250625193858_IncludeFailureReason.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailQueue.API.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludeFailureReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FailureReason",
+                table: "EmailTasks",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailureReason",
+                table: "EmailTasks");
+        }
+    }
+}
