e0bc44ab36b368c3bff80559d67fffb6d54699ed
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-06-25 15:39:31 -0400

    Add EF migration

--- src/EmailQueue.API/Database/Migrations/20250625193858_IncludeFailureReason.Designer.cs
+++ src/EmailQueue.API/Database/Migrations/20250625193858_IncludeFailureReason.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmailQueue.API.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmailQueue.API.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250625193858_IncludeFailureReason")]
+    partial class IncludeFailureReason
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
