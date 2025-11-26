6774a8f271dce3e8ea59d9f83531dfe8ebc28544
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-11-26 12:48:07 -0500

    Upgrade to .NET 10

--- src/EmailQueue.API/Database/DbRepository.cs
+++ src/EmailQueue.API/Database/DbRepository.cs
@@ -5,55 +5,55 @@ namespace EmailQueue.API.Database;
 
 internal static class DbRepository
 {
-    public static async Task SaveBatchAsync(this AppDbContext db, params EmailTask[] emailTasksList)
+    extension(AppDbContext db)
     {
-        await db.EmailTasks.AddRangeAsync(emailTasksList);
-        await db.SaveChangesAsync();
+        public async Task SaveBatchAsync(params EmailTask[] emailTasksList)
+        {
+            await db.EmailTasks.AddRangeAsync(emailTasksList);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<List<BatchStatusView>> GetAllBatchesAsync(Guid clientId) =>
+            await QueryByClientId(db, clientId)
+                .SelectBatchStatus()
+                .OrderByDescending(g => g.CreatedAt)
+                .ToListAsync();
+
+        public async Task<List<EmailTaskStatusView>> GetBatchDetailsAsync(Guid clientId, Guid batchId) =>
+            await QueryByClientId(db, clientId)
+                .Where(t => t.BatchId == batchId)
+                .OrderBy(t => t.CreatedAt)
+                .Select(t => new EmailTaskStatusView(t))
+                .ToListAsync();
+
+        public async Task<List<EmailTaskStatusView>> GetBatchFailedItemsAsync(Guid clientId, Guid batchId) =>
+            await QueryByClientId(db, clientId)
+                .Where(t =>
+                    t.BatchId == batchId &&
+                    (t.Status == nameof(EmailStatus.Failed) || t.Status == nameof(EmailStatus.Skipped)))
+                .OrderBy(t => t.CreatedAt)
+                .Select(t => new EmailTaskStatusView(t))
+                .ToListAsync();
+
+        public async Task<BatchStatusView?> GetBatchStatusAsync(Guid clientId, Guid batchId) =>
+            await QueryByClientId(db, clientId)
+                .Where(t => t.BatchId == batchId)
+                .SelectBatchStatus()
+                .SingleOrDefaultAsync();
+
+        public async Task<List<EmailTask>> GetQueuedItems() =>
+            await db.EmailTasks
+                .Where(t => t.Status == nameof(EmailStatus.Queued))
+                .OrderBy(t => t.CreatedAt)
+                .ToListAsync();
+
+        public async Task<int> GetMaxCounter() =>
+            await db.EmailTasks.DefaultIfEmpty()
+                .MaxAsync(t => t == null ? 0 : t.Counter);
+
+        private IQueryable<EmailTask> QueryByClientId(Guid clientId) =>
+            db.EmailTasks.Where(t => t.ClientId == clientId);
     }
-
-    public static async Task<List<BatchStatusView>> GetAllBatchesAsync(this AppDbContext db, Guid clientId) =>
-        await QueryByClientId(db, clientId)
-            .SelectBatchStatus()
-            .OrderByDescending(g => g.CreatedAt)
-            .ToListAsync();
-
-    public static async Task<List<EmailTaskStatusView>> GetBatchDetailsAsync(this AppDbContext db, Guid clientId,
-        Guid batchId) =>
-        await QueryByClientId(db, clientId)
-            .Where(t => t.BatchId == batchId)
-            .OrderBy(t => t.CreatedAt)
-            .Select(t => new EmailTaskStatusView(t))
-            .ToListAsync();
-
-    public static async Task<List<EmailTaskStatusView>> GetBatchFailedItemsAsync(this AppDbContext db, Guid clientId,
-        Guid batchId) =>
-        await QueryByClientId(db, clientId)
-            .Where(t =>
-                t.BatchId == batchId &&
-                (t.Status == nameof(EmailStatus.Failed) || t.Status == nameof(EmailStatus.Skipped)))
-            .OrderBy(t => t.CreatedAt)
-            .Select(t => new EmailTaskStatusView(t))
-            .ToListAsync();
-
-    public static async Task<BatchStatusView?> GetBatchStatusAsync(this AppDbContext db, Guid clientId, Guid batchId) =>
-        await QueryByClientId(db, clientId)
-            .Where(t => t.BatchId == batchId)
-            .SelectBatchStatus()
-            .SingleOrDefaultAsync();
-
-    public static async Task<List<EmailTask>> GetQueuedItems(this AppDbContext dbContext) =>
-        await dbContext.EmailTasks
-            .Where(t => t.Status == nameof(EmailStatus.Queued))
-            .OrderBy(t => t.CreatedAt)
-            .ToListAsync();
-
-    public static async Task<int> GetMaxCounter(this AppDbContext dbContext) =>
-        await dbContext.EmailTasks.DefaultIfEmpty()
-            .MaxAsync(t => t == null ? 0 : t.Counter);
-
-    // Internal methods
-    private static IQueryable<EmailTask> QueryByClientId(this AppDbContext db, Guid clientId) =>
-        db.EmailTasks.Where(t => t.ClientId == clientId);
 
     private static IQueryable<BatchStatusView> SelectBatchStatus(this IQueryable<EmailTask> emailTasks) => emailTasks
         .GroupBy(t => t.BatchId)
