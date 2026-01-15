e03e9ed98cf7d33c933a4f5aa7a5a5c1eab782f1
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-01-15 16:57:23 -0500

    Bump version: 2026.1.15

--- samples/Sample.WebApp/Settings/AppSettings.cs
+++ samples/Sample.WebApp/Settings/AppSettings.cs
@@ -4,20 +4,21 @@ namespace Sample.WebApp.Settings;
 
 public static class AppSettings
 {
-    public static string? InformationalVersion { get; private set; }
-    public static string? InformationalBuild { get; private set; }
+    public static string? Version { get; private set; }
     public const string DateTimeFormat = "d\u2011MMM\u2011yyyy h:mm:ss\u00a0tt";
 
     public static void BindSettings(this WebApplicationBuilder builder)
     {
+        Version = GetVersion();
         builder.Services.AddOptions<EmailQueueApi>().BindConfiguration(configSectionPath: nameof(EmailQueueApi));
+    }
 
-        // App version
-        var segments = (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-            .InformationalVersion ?? Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "").Split('+');
-        InformationalVersion = segments[0];
-        if (segments.Length > 0 && segments[1].Length > 0)
-            InformationalBuild = segments[1][..Math.Min(7, segments[1].Length)];
+    private static string GetVersion()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        var segments = (entryAssembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion ?? entryAssembly?.GetName().Version?.ToString() ?? "").Split('+');
+        return segments[0] + (segments.Length > 0 ? $"+{segments[1][..Math.Min(7, segments[1].Length)]}" : "");
     }
 }
 
